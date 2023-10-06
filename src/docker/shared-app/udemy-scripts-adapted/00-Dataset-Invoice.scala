3bdbc4d7ae8b8fdfe528bcca7f708e97f05f8d3f
jbprek <jbprek@gmail.com>
2023-10-06 12:48:40 +0100

    WIP

--- src/docker/shared-app/udemy-scripts-adapted/00-Dataset-Invoice.scala
+++ src/docker/shared-app/udemy-scripts-adapted/00-Dataset-Invoice.scala
@@ -7,3 +7,7 @@ val invoiceDF = spark.read.format("csv").
   load("/data/invoices.csv")
 
 invoiceDF.printSchema
+
+// Mini set 0.001 %
+val invDF = invoiceDF.filter($"Country" =!= "United Kingdom").
+sample(0.0011, 3651482897L)
