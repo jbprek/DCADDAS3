4ef497a1d336cdac6be31b2e5dc60bfbf14b693c
jbprek <jbprek@gmail.com>
2023-10-06 13:24:02 +0100

    WIP

--- src/docker/shared-app/udemy-scripts-adapted/00-Dataset-Sales-Simple.scala
+++ src/docker/shared-app/udemy-scripts-adapted/00-Dataset-Sales-Simple.scala
@@ -6,8 +6,8 @@ val salesInDF = spark.read.format("csv").
   option("header", "true").option("inferSchema", "true").
   load("/data/sales-simple.csv")
 
-salesDF.printSchema
-
 // Add autoincrement orderId column
 
 val salesDF = salesInDF.withColumn("orderId",monotonically_increasing_id())
+
+salesDF.printSchema
