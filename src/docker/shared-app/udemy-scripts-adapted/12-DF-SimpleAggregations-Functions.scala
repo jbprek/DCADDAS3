243dffdf6efad35b98fa411b3dc035bf5aa49d9f
jbprek <jbprek@gmail.com>
2023-10-06 07:01:39 +0100

    WIP

--- src/docker/shared-app/udemy-scripts-adapted/12-DF-SimpleAggregations-Functions.scala
+++ src/docker/shared-app/udemy-scripts-adapted/12-DF-SimpleAggregations-Functions.scala
@@ -9,32 +9,39 @@ import org.apache.spark.sql.types._
  */
 
 /* SIMPLE Aggregations */
-invoiceDF.select(
+invDF.select(
   count("*").as("Count *"),
   sum("Quantity").as("TotalQuantity"),
   avg("UnitPrice").as("AvgPrice"),
-  countDistinct("InvoiceNo").as("CountDistinct")
+  countDistinct("InvoiceNo").as("CountDistinct"),
+  countDistinct("Country").as("Country Distinct"),
+  countDistinct("StockCode").as("StockCode Distinct"),
+  countDistinct("Description").as("Description: Distinct"),
+  countDistinct("CustomerID").as("CustomerID:: Distinct")
 ).show()
 
 //  SOS note that unlikely most of the other functions that expect Column argument,
 //  aggregate functions have overloaded methods that support both String and Column
 //  for a column argument.
-invoiceDF.select(
+invDF.select(
   count($"*").as("Count *"),
   sum($"Quantity").as("TotalQuantity"),
   avg($"UnitPrice").as("AvgPrice"),
-  countDistinct($"InvoiceNo").as("CountDistinct")
+  countDistinct($"InvoiceNo").as("CountDistinct"),
+  countDistinct ($"StockCode").as("StockCode Distinct"),
+  countDistinct($"Description").as("Description Distinct"),
+  countDistinct($"CustomerID").as("CustomerID Distinct")
 ).show()
 
 // Usign expr
-invoiceDF.selectExpr(
+invDF.selectExpr(
   "count(1) as `count 1`",
   "count(StockCode) as `count field`",
   "sum(Quantity) as TotalQuantity",
   "avg(UnitPrice) as AvgPrice"
 ).show()
 // SQL
-invoiceDF.createOrReplaceTempView("sales")
+invDF.createOrReplaceTempView("sales")
 val summarySQL = spark.sql(
   """
     |SELECT Country, InvoiceNo,
