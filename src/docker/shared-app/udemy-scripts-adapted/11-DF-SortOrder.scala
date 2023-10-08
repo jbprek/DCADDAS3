79f68b38f1afe216bc9bd88988cb50d5389dde0a
jbprek <jbprek@gmail.com>
2023-10-08 09:55:13 +0100

    WIP

--- src/docker/shared-app/udemy-scripts-adapted/11-DF-SortOrder.scala
+++ src/docker/shared-app/udemy-scripts-adapted/11-DF-SortOrder.scala
@@ -2,7 +2,16 @@ import org.apache.spark.sql.functions._
 import org.apache.spark.sql.types._
 
 /* Setup */
-val df =  personDF.na.drop("any")
+
+var data = List(
+  ("John", "1990-02-01"),
+  ("John", "1967-03-15"),
+  ("Bill", "2002-06-09"),
+  ("Sean", "1980-08-23"),
+  ("Sophie", "1980-08-23")
+)
+val df = data.toDF("name","dob")
+
 
 /**
  * def sort(sortExprs: Column*): Dataset[T] :Returns a new Dataset sorted by the given expressions.
@@ -11,7 +20,6 @@ val df =  personDF.na.drop("any")
  * orderBy same syntax as sort
  */
 df.sort($"dob".desc, $"name".asc).show
-df.sort(expr("dob desc"), expr("name")).show
 
-// SOS With the use of String cannot specify sort order
+// SOS With the use of String cannot specify sort order it's always ascending
 df.sort("dob","name").show
