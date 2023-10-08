c0dd41b769cfcaceb67b34d4c0d2e0afdb372ad4
jbprek <jbprek@gmail.com>
2023-10-08 09:22:16 +0100

    WIP

--- src/docker/shared-app/udemy-scripts-adapted/00-Dataset-Sales-Simple.scala
+++ src/docker/shared-app/udemy-scripts-adapted/00-Dataset-Sales-Simple.scala
@@ -2,9 +2,7 @@ import org.apache.spark.sql.functions._
 import org.apache.spark.sql.types._
 import org.apache.spark.sql._
 
-val salesDF = spark.read.format("csv").
-  option("header", "true").option("inferSchema", "true").
-  load("/data/sales-simple.csv")
+val salesDF = spark.read.format("csv").option("header", "true").option("inferSchema", "true").load("/data/sales-simple.csv")
 
 salesDF.createOrReplaceTempView("sales")
 
